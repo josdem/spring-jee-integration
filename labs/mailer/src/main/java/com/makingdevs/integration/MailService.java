5236e0248b7aea5158c2420a4c8116b45fcf86be
neodevelop <neodevelop@gmail.com>
2014-11-17 22:59:44 -0600

    Agregando elementos iniciales para la integración del correo

---
+++ labs/mailer/src/main/java/com/makingdevs/integration/MailService.java
@@ -0,0 +1,20 @@
+package com.makingdevs.integration;
+
+import java.util.Map;
+
+import javax.mail.MessagingException;
+
+import org.springframework.core.io.FileSystemResource;
+
+public interface MailService {
+  void sendMail(String email, String message);
+  void sendMimeMail(String email, String message, String subject);
+  void sendMailWithAttach(String email, String message, String subject,
+      FileSystemResource attach) throws MessagingException;
+  void sendMailWithInline(String email, String message, String subject,
+      FileSystemResource inline) throws MessagingException;
+  void sendMailWithEngine(String email, Map<String, Object> model,
+      String subject, String template);
+  void sendMailWithVelocityEngine(String email, Map<String, Object> model,
+      String subject, String template);
+}
